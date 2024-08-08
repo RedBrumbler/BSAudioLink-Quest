2d56ddde57b00482c1a93f4df51065287bf4291c
RedBrumbler <redbrumbler.official+github@gmail.com>
2024-08-08 02:11:15 +0200

    Zenjectify more stuff

--- include/AssetBundleManager.hpp
+++ include/AssetBundleManager.hpp
@@ -10,18 +10,16 @@
 
 #include <functional>
 
-DECLARE_CLASS_CODEGEN(AudioLink, AssetBundleManager, Il2CppObject,
+DECLARE_CLASS_CODEGEN_INTERFACES(AudioLink, AssetBundleManager, Il2CppObject, classof(System::IDisposable*),
     DECLARE_PRIVATE_FIELD(UnityEngine::AssetBundle*, _bundle);
     DECLARE_PRIVATE_FIELD(UnityEngine::Material*, _material);
     DECLARE_PRIVATE_FIELD(UnityEngine::RenderTexture*, _renderTexture);
 
     DECLARE_INSTANCE_METHOD(UnityEngine::Material*, get_material);
     DECLARE_INSTANCE_METHOD(UnityEngine::RenderTexture*, get_renderTexture);
+    DECLARE_OVERRIDE_METHOD(void, Dispose, il2cpp_utils::il2cpp_type_check::MetadataGetter<&System::IDisposable::Dispose>::get());
     public:
-        static AssetBundleManager* get_instance();
         void Load();
-        void RestartGame();
         DECLARE_DEFAULT_CTOR();
     private:
-        static SafePtr<AssetBundleManager> instance;
 )
--- src/AssetBundleManager.cpp
+++ src/AssetBundleManager.cpp
@@ -11,15 +11,6 @@ DEFINE_TYPE(AudioLink, AssetBundleManager);
 extern Logger& getLogger();
 
 namespace AudioLink {
-    SafePtr<AssetBundleManager> AssetBundleManager::instance;
-    
-    AssetBundleManager* AssetBundleManager::get_instance() {
-        if (!instance) {
-            instance = AssetBundleManager::New_ctor();
-        }
-        return instance.ptr();
-    }
-
     UnityEngine::Material* AssetBundleManager::get_material() {
         return _material;
     }
@@ -32,19 +23,23 @@ namespace AudioLink {
         if (_bundle && _bundle->m_CachedPtr.m_value) return;
         using AssetBundle_LoadFromMemory = function_ptr_t<UnityEngine::AssetBundle*, ArrayW<uint8_t>, int>;
         static auto assetBundle_LoadFromMemory = reinterpret_cast<AssetBundle_LoadFromMemory>(il2cpp_functions::resolve_icall("UnityEngine.AssetBundle::LoadFromMemory_Internal"));
-    
+
         static auto _bundle_info = il2cpp_functions::class_get_field_from_name(klass, "_bundle");
         static auto _material_info = il2cpp_functions::class_get_field_from_name(klass, "_material");
         static auto _renderTexture_info = il2cpp_functions::class_get_field_from_name(klass, "_renderTexture");
 
-        il2cpp_functions::field_set_value_object(this, _bundle_info, assetBundle_LoadFromMemory(IncludedAssets::Bundle, 0));
-        il2cpp_functions::field_set_value_object(this, _material_info, _bundle->LoadAsset<UnityEngine::Material*>("assets/audiolink/materials/mat_audiolink.mat"));
+        auto bundle = assetBundle_LoadFromMemory(IncludedAssets::Bundle, 0);
+        il2cpp_functions::field_set_value_object(this, _bundle_info, bundle);
 
-        auto rt = _bundle->LoadAsset("assets/audiolink/rendertextures/rt_audiolink.asset");
+        auto material = bundle->LoadAsset<UnityEngine::Material*>("assets/audiolink/materials/mat_audiolink.mat");
+        il2cpp_functions::field_set_value_object(this, _material_info, material);
+
+        auto rt = bundle->LoadAsset("assets/audiolink/rendertextures/rt_audiolink.asset");
         il2cpp_functions::field_set_value_object(this, _renderTexture_info, rt);
+        getLogger().flush();
     }
 
-    void AssetBundleManager::RestartGame() {
+    void AssetBundleManager::Dispose() {
         getLogger().info("AssetBundleManager RestartGame");
         static auto _bundle_info = il2cpp_functions::class_get_field_from_name(klass, "_bundle");
         static auto _material_info = il2cpp_functions::class_get_field_from_name(klass, "_material");
